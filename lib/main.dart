5431790ace368bac463d4a074f3c9de747fcc9df
sonyebin <sonyeabin38@naver.com>
2024-09-04 01:19:12 +0900

    푸쉬 테스트 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Scaffold(
+
+      )
+    );
   }
 }
